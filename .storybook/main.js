75181d6072799eae15c5c8b61a03692e2a6a2a1f
Alquipo <alquiponeto@outlook.com.br>
2020-12-23 16:12:44 -0300

    feat: storybook

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  stories: [
+    '../src/components/**/stories.mdx',
+    '../src/components/**/stories.@(js|jsx|ts|tsx)'
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs/register'
+  ]
+}
---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,19 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { withKnobs, text } from '@storybook/addon-knobs'
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  decorators: [withKnobs]
+} as Meta
+
+export const Basic: Story = () => (
+  <Main
+    title={text('Title', 'React Avançado')}
+    description={text(
+      'Description',
+      'TypeScript, ReactJS, NextJS e Styled Components'
+    )}
+  />
+)
