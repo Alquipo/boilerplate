9268daea4e90708d3ca0807ffc2b36207191101c
Alquipo <alquiponeto@outlook.com.br>
2020-12-23 16:28:59 -0300

    fix: controls sotrybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,9 +3,5 @@ module.exports = {
     '../src/components/**/stories.mdx',
     '../src/components/**/stories.@(js|jsx|ts|tsx)'
   ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-actions',
-    '@storybook/addon-knobs/register'
-  ]
+  addons: ['@storybook/addon-essentials']
 }
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,19 +1,10 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { withKnobs, text } from '@storybook/addon-knobs'
+
 import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main,
-  decorators: [withKnobs]
+  component: Main
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e Styled Components'
-    )}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
