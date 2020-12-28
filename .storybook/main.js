f674775e466b34b8922c2a2babcb6fd5df873165
Alquipo <alquiponeto@outlook.com.br>
2020-12-27 22:50:23 -0300

    fix: errors boilerplate

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,5 +3,9 @@ module.exports = {
     '../src/components/**/stories.mdx',
     '../src/components/**/stories.@(js|jsx|ts|tsx)'
   ],
-  addons: ['@storybook/addon-essentials']
+  addons: ['@storybook/addon-essentials'],
+  babel: async (options) => ({
+    ...options,
+    plugins: [...options.plugins, '@babel/plugin-transform-react-jsx']
+  })
 }
