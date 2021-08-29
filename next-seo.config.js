50b62bd84a51c708ddb95d6c23050469e285b9e3
Alquipo <alquiponeto@outlook.com.br>
2021-08-29 20:15:33 -0300

    fix: dependencies

---
+++ next-seo.config.js
@@ -0,0 +1,14 @@
+export default {
+  openGraph: {
+    type: 'Boilerplate',
+    locale: 'en_US',
+    url: 'https://alquipo.dev/',
+    site_name: 'Boilerplate',
+    title: 'Boilerplate'
+  },
+  twitter: {
+    handle: '@alquiponeto',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
