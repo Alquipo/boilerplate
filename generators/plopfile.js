30a6743ce50ebd740624757379ed43977a465224
Alquipo <alquiponeto@outlook.com.br>
2021-11-20 00:26:53 -0300

    fix: clear jest and storybook

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -18,16 +18,6 @@ module.exports = (plop) => {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/styles.ts',
         templateFile: 'templates/styles.ts.hbs'
-      },
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}/stories.tsx',
-        templateFile: 'templates/stories.tsx.hbs'
-      },
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}/test.tsx',
-        templateFile: 'templates/test.tsx.hbs'
       }
     ]
   })
