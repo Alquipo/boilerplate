37cce03dee3c44c4f4de00286c8569d1ac5141b7
Alquipo Neto <alquiponeto@outlook.com.br>
2022-01-16 18:24:11 -0300

    chore: update dependencies

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
-  swcMinify: true,
   pwa: {
     dest: 'public',
     disable: !isProd
