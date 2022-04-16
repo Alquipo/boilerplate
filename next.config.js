e680f9ee5952c4397f702b81f567fdf6db7454eb
Alquipo Neto <alquiponeto@outlook.com.br>
2022-04-16 13:29:47 -0300

    fix: update dependencies

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true
+  },
   pwa: {
     dest: 'public',
     disable: !isProd
