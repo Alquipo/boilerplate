be3489107bbd59b93c6ed8f9cc4397e9a004f691
Alquipo Neto <alquiponeto@outlook.com.br>
2022-10-12 16:50:47 -0300

    fix: update storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,16 +1,6 @@
-import * as NextImage from "next/image";
-
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from '../src/styles/global'
 import theme from '../src/styles/theme'
-
-Object.defineProperty(NextImage, 'default', {
-  configurable: true,
-  value: (props) => {
-    const { objectFit, ...rest } = props
-    return <img {...rest} />
-  }
-})
 
 export const decorators = [
   (Story) => (
